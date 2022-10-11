e173a902ee441ca12909637730f632ffd81761c3
oussama <oussama.hihoud@student.rhb.be>
2022-10-11 19:39:07 +0200

    spaceinvader finished

--- spaceInvader.js
+++ spaceInvader.js
@@ -11,3 +11,27 @@ context.beginPath();
 context.rect(50, 50, 300, 300);
 context.stroke();
 context.fill();
+
+context.lineWidth = 1;
+context.fillStyle = "#66FF00";
+context.beginPath();
+context.rect(75, 275, 50, 50);
+context.fill();
+
+context.lineWidth = 1;
+context.fillStyle = "#66FF00";
+context.beginPath();
+context.rect(175, 275, 50, 50);
+context.fill();
+
+context.lineWidth = 1;
+context.fillStyle = "#66FF00";
+context.beginPath();
+context.rect(275, 275, 50, 50);
+context.fill();
+
+context.lineWidth = 1;
+context.fillStyle = "#66FF00";
+context.beginPath();
+context.rect(175, 125, 50, 50);
+context.fill();
