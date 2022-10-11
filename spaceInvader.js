6e4adc8de4e2f6d1379fe2ac50f3b7a070a11f30
oussama <oussama.hihoud@student.rhb.be>
2022-10-11 19:04:06 +0200

    background black rectangle

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,13 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+context.lineWidth = 1;
+context.fillStyle = "black";
+context.beginPath();
+context.rect(50, 50, 300, 300);
+context.stroke();
+context.fill();
